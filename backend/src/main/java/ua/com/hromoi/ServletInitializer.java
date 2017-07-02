bcefd87c918aa2a39d81fa839bb232d3dc586197
Viacheslav Khromoi <vyacheslavkhromoy@gmail.com>
2017-07-02 10:03:52 +0300

    initial commit

---
+++ backend/src/main/java/ua/com/hromoi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ua.com.hromoi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TaskManagerApplication.class);
+	}
+
+}
